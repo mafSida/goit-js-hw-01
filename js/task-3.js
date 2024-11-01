d07f155f55bc592944316239ab571d1fd1c39136
Daiana Kutasevych <dayasya2@gmail.com>
2024-11-01 18:37:18 +0000

    feat: Complete third homework task

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function getElementWidth(content, padding, border) {
-  let 
-  let 
-  return 
+  let numericalContent = Number.parseInt(content);
+  let numericalPadding = Number.parseInt(padding);
+  let numericalBorder = Number.parseInt(border);
+  let result = numericalContent + numericalPadding * 2 + numericalBorder * 2;
+  return `${result}px`;
 }
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
